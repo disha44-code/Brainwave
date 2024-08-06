90c0cad34365e91bd052c50df4baf74ce1ff5c25
disha <wd44.department@gmail.com>
2024-08-06 14:56:36 +0530

    Our services section completed

--- src/app/agency/page.js
+++ src/app/agency/page.js
@@ -2,6 +2,7 @@ import Banner from '@/components/agency/banner/banner'
 import ServiceCard from '@/components/agency/servicecard/Service_card'
 import Header from '@/components/header/header'
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 // import Script from 'next/script'; 
 
 
@@ -14,13 +15,46 @@ const Agency = () => {
       <Header/>
       <Banner/>
       </div>
-      <ServiceCard
-        imageSrc="/banner-vector-1.png" // Replace with the path to your image
-        title="Graphic Design"
-        description="With lots of unique blocks, you can easily build a page without coding. Build your next landing page."
-        link="#"
-        sx={{ margin: '20px', padding: '30px' }} // Example sx props
-      />
+        <div className='service-card-section'>
+          <Container>
+            <div className='service-card-title'>
+              <h6 className='subtitle text-center'>Our services</h6>
+              <h2 className='section-title'>We provide great services for our customers based on needs</h2>
+            </div>
+            <Row className='service-vector'>
+                <Col lg={4} >
+                <ServiceCard
+                  bgColor ="#68D585"
+                  imageSrc="/Designer-rafiki.png" // Replace with the path to your image
+                  title="Graphic Design"
+                  description="With lots of unique blocks, you can easily build a page without coding. Build your next landing page."
+                  link="#"
+                  sx={{ margin: '20px', padding: '30px' }} // Example sx props
+                />
+                </Col>
+                <Col lg={4} >
+                <ServiceCard
+                  bgColor ="#473BF0"
+                  imageSrc="/Programming-rafiki.png" // Replace with the path to your image
+                  title="Web Development"
+                  description="With lots of unique blocks, you can easily build a page without coding. Build your next landing page."
+                  link="#"
+                  sx={{ margin: '20px', padding: '30px' }} // Example sx props
+                />
+                </Col>
+                <Col lg={4} >
+                <ServiceCard
+                  bgColor ="#F64B4B"
+                  imageSrc="/Notes-rafiki.png" // Replace with the path to your image
+                  title="Content Writing"
+                  description="With lots of unique blocks, you can easily build a page without coding. Build your next landing page."
+                  link="#"
+                  sx={{ margin: '20px', padding: '30px' }} // Example sx props
+                />
+                </Col>
+            </Row>
+          </Container>
+        </div>
     </div>
     </>
   )
--- src/components/agency/servicecard/Service_card.js
+++ src/components/agency/servicecard/Service_card.js
@@ -6,20 +6,26 @@ import { Card } from 'react-bootstrap';
 
 const ServiceCard = ({ imageSrc, title, description, link, bgColor }) => {
   return (
-    <Card className='service-card' style={{ backgroundColor: {bgColor}, color: 'white', textAlign: 'center', borderRadius: '10px', padding:'24px' }}>
-      <Card.Body>
-        <div className="image-container" style={{ marginBottom: '15px' }}>
+    <Card className='service-card' style={{ backgroundColor: bgColor, color: 'white', textAlign: 'center', borderRadius: '10px', border:'none', height:'100%' }}>
+      <Card.Body style={{padding:0}}>
+        <div className="image-container">
           <Image
             src={imageSrc}
             alt="Graphic Design Illustration"
             className='img'
-            width={100}
-            height={100}
+            width={146}
+            height={135}
           />
         </div>
         <h4 className='card-title'>{title}</h4>
         <p className='card-para'>{description}</p>
-        <Link href="/" className='link'>Learn more &rarr;</Link>
+        <Link href="/" className='link'>Learn more 
+        <svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.40039 2L13.0004 7.2L7.40039 12.4" stroke="white" stroke-width="2" stroke-linecap="square" stroke-linejoin="bevel"/>
+<path d="M1 6.98571H12.2" stroke="white" stroke-width="2" stroke-linecap="square" stroke-linejoin="bevel"/>
+</svg>
+
+        </Link>
       </Card.Body>
     </Card>
   );
